99cce0df18203251bb017ceefd6334c132cd0163
alainrk <alain.narko@gmail.com>
2013-07-19 17:27:19 +0200

    files2

--- catchmouse.sh
+++ catchmouse.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-touch "REMOVEMEF98SEHIDXUKSG"
-sleep 2
-
 location=`xdotool getmouselocation 2>&1 |  sed -rn '${s/x:([0-9]+) y:([0-9]+) .*/\1 \2/p}'`
 IFS=' ' read -a array <<< "$location"
 oldx="${array[0]}"
@@ -15,7 +12,6 @@ while [[ true ]]; do
 	y="${array[1]}"
 	if [[ $x -ne $oldx || $y -ne $oldy ]]; then
 		./stop.sh
-		rm "REMOVEMEF98SEHIDXUKSG"
 		exit 0
 	fi
 
