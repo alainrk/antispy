b7326726b49594e7ef8b063ccf0a83f4bf91b8f1
alainrk <alain.narko@gmail.com>
2013-07-19 18:21:40 +0200

    OK!

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ antispy
 =======
 
 A Linux hack for leave a little bit safer your laptop and run to the toilet!
+  ---> LOCK mouse and keyboard [escape default button:\] 
--- antispy.sh
+++ antispy.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-sleep 2
-echo "3,2,1 -> GO"
+sleep 0.5
+echo '3'
+sleep 0.5
+echo '2'
+sleep 0.5
+echo '1'
+sleep 0.5
+echo 'GO'
+sleep 0.5
+
 cd $HOME/antispy/
 ./catchmouse.sh "$$" &
 key=`./keyboard "/dev/input/event3"`
 
-if [[ $key -eq 61 ]]; then
+if [[ $key -eq 41 ]]; then
 	killall catchmouse.sh
 	exit 0
 else
